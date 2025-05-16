79b250cfcbb061e869d724ca7623f1057d81d94a
CARREREOlivier <carrereolivier@yahoo.fr>
2025-05-16 15:33:55 +0200

    feat: amélioration du sommaire des articles dans la vue œuvre avec recherche et fallback

--- assets/js/filters/articlesFilter.js
+++ assets/js/filters/articlesFilter.js
@@ -1,9 +1,14 @@
 export function filterArticles(query) {
     const articles = document.querySelectorAll('#sommaire > div');
+    const noResultsMessage = document.getElementById('noResultsMessage');
+    let found = false;
     query = query.toLowerCase();
 
     articles.forEach(article => {
         const title = article.querySelector('a').textContent.toLowerCase();
         article.style.display = title.includes(query) ? '' : 'none';
     });
+    if (noResultsMessage) {
+        noResultsMessage.classList.toggle('hidden', found);
+    }
 }
